421ef9ab057c33f5919d510555a9c613f1150e70
Piyush Payodhi <ppayodhi@gmail.com>
2019-06-02 15:53:46 +0530

    Push to TFS

--- Log4NetProgram.cs
+++ Log4NetProgram.cs
@@ -22,5 +22,21 @@ namespace PiyushApps.Log4NetApp
 
 
         }
+
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            Logger.Debug("This is a Debug message");
+            Logger.Info("This is a Info message");
+            Logger.Warn("This is a Warning message");
+            Logger.Error("This is an Error message");
+            Logger.Fatal("This is a Fatal message");
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+        }
     }
 }
